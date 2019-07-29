64004f8da96613e7a1dd1c96835abed91cc82754
TanviPriyadarshini <14tp17@queensu.ca>
2019-07-29 16:49:16 -0400

    testing with new git setup

--- client/src/App.js
+++ client/src/App.js
@@ -52,22 +52,12 @@ const getStoryInfo = async storyId => {
   }
 };*/
 const App = () => {
-  const [_stories, setStories] = useState([]);
-  const [activePostIndex] = useState(0);
+  const [stories, setStories] = useState([]);
+  const [activePostIndex, setActivePost] = useState(0);
   const [fetching, setFetching] = useState(false);
   const inputRef = useRef();
-};
-// class App extends Component {
-  // constructor() {
-  //   console.log("Inside app.js constructor function");
-  //   super();
-  //   this.state = {
-  //     _stories: [],
-  //     activePostIndex: 0
-  //   };
-  // }
 
-  onSwipeLeft = () => {
+  const onSwipeLeft = () => {
     console.log("I was swiped left.");
     const lastChildIndex = this.state._stories.length - 1;
     let { activePostIndex } = this.state;
@@ -75,16 +65,14 @@ const App = () => {
 
     activePostIndex = Math.min(Math.max(activePostIndex, 0), lastChildIndex);
 
-    if (lastChildIndex - 5 === activePostIndex) {
+    if (activePostIndex >= lastChildIndex - 5 && !fetching) {
       this.fetchData();
     }
 
-    this.setState({
-      activePostIndex
-    });
+    setActivePost(activePostIndex);
   };
 
-  onSwipeRight = () => {
+  const onSwipeRight = () => {
     console.log("I was swiped right.");
     let { activePostIndex } = this.state;
     activePostIndex = activePostIndex - 1;
@@ -93,12 +81,10 @@ const App = () => {
       this.state._stories.length - 1
     );
 
-    this.setState({
-      activePostIndex
-    });
+    setActivePost(activePostIndex);
   };
 
-  fetchData = async () => {
+  const fetchData = async () => {
     console.log("fetch called");
     const storyIds = await getNewStories();
     let actions = storyIds.slice(0, 10).map(getStoryInfo);
@@ -107,139 +93,47 @@ const App = () => {
     console.log("results", filterResults);
     const filteredUrls = filterResults.map(data => data.url);
     console.log("list of all filtered urls", filteredUrls);
-
-    this.setState({
-      _stories: filterResults
-    });
-    // const rank = index + 1;
-    // return await new Promise(resolve => {
-    //   Api.fetch(`/item/${id}`, {
-    //     then(data) {
-    //       let item = data;
-    //       // add the rank since it does not exist yet
-    //       item.rank = rank;
-    //       resolve(item);
-    //     }
-    //   });
-  };
-  /*Api.fetch(
-      `/newstories`,
-      {
-        context: this,
-        then(storyIds) {
-          console.log(`This is logged storyIds ${storyIds}`);
-          this.fetchNewStories(storyIds);
-        }
-      },
-      { mode: "cors" }
-    );*/
-  //};
-
-  // componentDidMount() {
-    useEffect( () =>{
-    console.log("componentDidMount called");
-    this.fetchData();})
-    // }
-
-  /*fetchNewStories = async storyIds => {
-    let actions = storyIds.slice(0, 10).map(this.fetchSingleStory);
-    let results = await Promise.all(actions);
-    console.log("results", results);
-    this.setState({
-      _stories: results
-    });
   };
 
-  fetchSingleStory = async (id, index) => {
-    const rank = index + 1;
-    return await new Promise(resolve => {
-      Api.fetch(`/item/${id}`, {
-        then(data) {
-          let item = data;
-          // add the rank since it does not exist yet
-          item.rank = rank;
-          resolve(item);
-        }
-      });
-    });
-  };*/
+  useEffect(() => {
+    this.fetchData();
+  }, []);
 
-  // render() {
-  //   console.log("this.activePostIndex", this.state.activePostIndex);
-  //   const activePost = this.state._stories[this.state.activePostIndex];
-  //   const _stories = this.state._stories;
-  //   console.log("typeOf activePost", typeof activePost);
-  //   console.log("activePost", activePost);
-  //   console.log("_stories", _stories);
-    //metadata processing
-    /*if (activePost) {
-      console.log("url extracted", activePost.url);
-      urlMetadata(activePost.url).then(function(metadata) {
-        console.log(metadata);
-      });
-    }*/
-    return (
-    console.log("this.activePostIndex", this.state.activePostIndex);
+  console.log("this.activePostIndex", this.state.activePostIndex);
+  const activePost = this.state._stories[this.state.activePostIndex];
+  const _stories = this.state._stories;
+  console.log("this.activePostIndex", this.state.activePostIndex);
   const activePost = this.state._stories[this.state.activePostIndex];
   const _stories = this.state._stories;
   console.log("typeOf activePost", typeof activePost);
   console.log("activePost", activePost);
   console.log("_stories", _stories);
-      <div className="background-gradient">
-        <div className="split left" onClick={this.onSwipeRight} />
-        <div className="split right" onClick={this.onSwipeLeft} />
-        <div className="snap-container">
-          {activePost && (
-            <div
-              className="snap-child"
-              onSwipeRight={() => this.onSwipeRight(this)}
-              onSwipeLeft={() => this.onSwipeLeft(this)}
-            >
-              <a href={activePost.url}>
-                <strong>{activePost.title}</strong>
-              </a>
-              {/* {activePost.metadata.results.data.ogImage && (
+
+  return (
+    <div className="background-gradient">
+      <div className="split left" onClick={this.onSwipeRight} />
+      <div className="split right" onClick={this.onSwipeLeft} />
+      <div className="snap-container">
+        {activePost && (
+          <div
+            className="snap-child"
+            onSwipeRight={() => this.onSwipeRight(this)}
+            onSwipeLeft={() => this.onSwipeLeft(this)}
+          >
+            <a href={activePost.url}>
+              <strong>{activePost.title}</strong>
+            </a>
+            {/* {activePost.metadata.results.data.ogImage && (
                 <span>{activePost.metadata.results.data.ogImage.url}</span>
                 )} */}
 
-              <br />
-              <hr />
-              <span>-{activePost.by}</span>
-            </div>
-          )}
-        </div>
-        {/*{_stories.map(story => (
-            <Card
-              onSwipeRight={this.onSwipeRight}
-              onSwipeLeft={this.onSwipeLeft}
-              style={{ transform: "none" }}
-              key={story.rank}
-            >
-              <a href={story.url}>
-                <strong>{story.title}</strong>
-              </a>
-              <br />
-              <hr />
-              <span>-{story.by}</span>
-            </Card>
-          ))}
-        </CardWrapper>
-        {activePost && (
-          <div>
-            <div className="note">
-              <a href={activePost.url}>
-                <strong>{activePost.title}</strong>
-              </a>
-
-              <br />
-              <hr />
-              <span>-{activePost.by}</span>
-            </div>
+            <br />
+            <hr />
+            <span>-{activePost.by}</span>
           </div>
-        )}*/}
+        )}
       </div>
-    );
-  // }
-// }
-
+    </div>
+  );
+};
 export default App;
