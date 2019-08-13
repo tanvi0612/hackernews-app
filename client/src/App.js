717d9b6269a3f365fd2295a0c77fcd602bf59d1c
tanvi0612 <tanvi@lumierevr.com>
2019-08-13 18:09:27 -0400

    Img can be received at the front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,11 @@
-import React, { useState, useEffect, useRef } from "react";
-import rebase from "re-base";
-import firebase from "firebase/app";
-import database from "firebase/database";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-const axios = require("axios");
 
-const HN_DATABASE_URL = "https://hacker-news.firebaseio.com/";
-const HN_VERSION = "v0";
+let pageNumber = 1;
 
-const getNewStories = async pageNo => {
-  const req = await fetch(
-    HN_DATABASE_URL + "/" + HN_VERSION + "/newstories.json"
-  );
-  const res = await req.json();
-  return res;
-  // const req = await axios
-  //   .get(
-  //     "https://api.hnpwa.com/v0/newest/6.json"
-  //     // "https://hacker-news.firebaseio.com/v0/newstories.json"
-  //   )
-  //   .then(json => {
-  //     console.log(json.data);
-  //   })
-  //   .catch(function(error) {
-  //     // handle error
-  //     console.log(error);
-  //   });
-  // const res = await req.json();
-  // console.log("req at line 27", req);
-  // return res;
-};
-
-const getStoryInfo = async storyId => {
-  const req = await fetch(
-    HN_DATABASE_URL + "/" + HN_VERSION + "/item/" + storyId + ".json"
-  );
+const getNewStories = async () => {
+  const req = await fetch(`https://api.hnpwa.com/v0/news/${pageNumber}.json`);
+  pageNumber++;
   const res = await req.json();
   return res;
 };
@@ -47,8 +18,7 @@ const App = () => {
   const onSwipeLeft = () => {
     console.log("I was swiped left.");
     const lastChildIndex = stories.length - 1;
-    // setActivePostIndex(activePostIndex + 1);
-    // console.log("setActivePostIndex in line 35", activePostIndex);
+
     setActivePostIndex(Math.min(Math.max(activePostIndex, 0), lastChildIndex));
     if (activePostIndex >= lastChildIndex - 5 && !fetching) {
       fetchData();
@@ -59,7 +29,6 @@ const App = () => {
 
   const onSwipeRight = () => {
     console.log("I was swiped right.");
-    //  setActivePostIndex(activePostIndex - 1);
     if (activePostIndex < 0) {
       return;
     }
@@ -72,28 +41,38 @@ const App = () => {
   const fetchData = async () => {
     console.log("fetch called");
     setFetching(true);
-    const storyIds = await getNewStories();
-    console.log("storyIds", storyIds);
-    let actions = storyIds.slice(0, 10).map(getStoryInfo);
-    let results = await Promise.all(actions);
-    let filterResults = results.filter(item => !!item);
-    console.log("results", filterResults);
-    setStories(filterResults);
-    // const filteredUrls = filterResults.map(data => data.url);
-    // console.log("list of all filtered urls", filteredUrls);
+    const stories = await getNewStories(pageNumber);
+    setStories(previousStories => [...previousStories, ...stories]);
+    console.log("stories: ", stories);
+    setFetching(false);
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  // console.log("this.activePostIndex", this.state.activePostIndex);
   const activePost = stories[activePostIndex];
-  // const stories = this.state._stories;
-  // console.log("this.activePostIndex", this.state.activePostIndex);
-  // const stories = this.state._stories;
+
+  const fetchFunc = async () => {
+    if (!activePost) {
+      return;
+    }
+    let fetchRes = await fetch(
+      `http://localhost:5000/?website=${activePost.url}`
+    );
+    let res = await fetchRes.json();
+    let img = res.results.data.ogImage.url;
+    console.log("fetch res", res.results.data.ogImage.url);
+    console.log("img", img);
+    return img;
+  };
+
+  let showImageUrl = fetchFunc();
+  // console.log("typeof fetchFunc", fetchFunc);
+
   console.log("typeOf activePost", typeof activePost);
   console.log("activePost", activePost);
+  //console.log("fetchRes", fetchRes);
   console.log("stories", stories);
   console.log("activePostIndex", activePostIndex);
 
@@ -109,16 +88,13 @@ const App = () => {
         {activePost && (
           <div
             className="snap-child"
-            onSwipeRight={() => onSwipeRight(this)}
-            onSwipeLeft={() => onSwipeLeft(this)}
+            // onSwipeRight="onSwipeRight()"
+            // onSwipeLeft="onSwipeLeft()"
           >
             <a href={activePost.url}>
               <strong>{activePost.title}</strong>
             </a>
-            {/* {activePost.metadata.results.data.ogImage && (
-                <span>{activePost.metadata.results.data.ogImage.url}</span>
-                )} */}
-
+            {/* <img src={showImageUrl} alt="Alt text" /> */}
             <br />
             <hr />
             <span>-{activePost.by}</span>
